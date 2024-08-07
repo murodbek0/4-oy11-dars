08ca730e6f3597cce4474ebb377ef6539f6df4ea
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-08-07 14:57:03 +0500

    11 dars

---
+++ js/index.js
@@ -0,0 +1,246 @@
+// 1 misol
+
+// let res = [1,2,3,4,5,6,7];
+// let counter = 0
+// function name(arg) {
+//    for (const iterator of arg) {
+//     if(iterator %  2 == 1){
+//        counter++
+//     }
+
+//     }
+//     return counter
+// }
+// console.log(name(res));
+
+// 2 misol
+
+// let  arr= "Sa3lo1m"
+// function  string(arr){
+// let son =[]
+// for (const iterator of arr) {
+//     if(iterator > 0){
+//     son.push(iterator)
+//     }
+// }
+// return son
+// }
+// console.log(string(arr));
+
+// 3 misol
+// let str = 'salom xayr';
+
+// function sum(arg) {
+//   let res = arg.split(' ').filter(res => res.length > 0)
+//   return res.length;
+// }
+
+// console.log(sum(str));
+
+// 4 misol
+
+// let res = [1, 2, 3, 45, 6, 7];
+
+// function name(arg) {
+//   let min = arg[0];
+//   for (const iterator of arg) {
+//     if (iterator < min)
+//     iterator = min;
+//   }
+//   return min;
+// }
+
+// console.log(name(res));
+
+// 5 misol
+
+// let str = 'salom';
+
+// function sum(arg) {
+//     let reversed = '';
+
+//     for (let i = arg.length - 1; i >= 0; i--) {
+//         reversed += arg[i];
+//     }
+
+//     return reversed;
+// }
+// console.log(sum(str));
+
+// 6 misol
+
+// let res = [1, 2, 3, 5, 6, 7];
+// function name(arg) {
+//   let sum = 1;
+//   for (const iterator of arg) {
+//    sum = sum * iterator
+
+// }
+// return sum
+// }
+
+// console.log(name(res));
+
+// 7 misol
+
+// let res = [1, 2, 3, 4, 5, 6, 7];
+// function name(arg) {
+//   return arg.sort((a, b) => b - a);
+// }
+// console.log(name(res));
+
+// 8 misol
+
+// let str = ('salom bolalar qalesilar');
+// function name(arg ,arg1) {
+//   return arg.replace(arg1 , '')
+
+// }
+
+// console.log(name(str , 'salom'));
+
+// 9 misol
+
+// let str = ('salom bolalar qalesilar');
+// function name(arg) {
+//   return arg.toUpperCase();
+
+// }
+
+// console.log(name(str));
+
+// 10 misol
+
+// let res = [1,2,3,4,5,6,7];
+// function name(arg) {
+//   let sum = []
+//    for (const iterator of arg) {
+//     if(iterator %  2 == 1){
+//       sum.push(iterator)
+//     }
+
+//     }
+//     return sum
+// }
+// console.log(name(res));
+
+// 11 misol
+
+// let res = ["salom", "xayr", "bolalar"];
+
+// function name(arg) {
+//   let res1 = "";
+//   for (const iterator of arg) {
+//     if (iterator.length > res1.length) {
+//       res1 = iterator;
+//     }
+//   }
+//   return res1;
+// }
+// console.log(name(res));
+
+// 12 misol
+
+// let res = [3,4,6,7,10];
+// function name(arg) {
+//   let sum = 0;
+//   for (const iterator of arg) {
+//     sum = sum + iterator
+
+//   }
+//   return sum
+// }
+// console.log(name(res));
+
+// 13 misol
+
+// let res = [-3,-4,6,7,10];
+//   let res1 = [];
+// function name(arg) {
+//   for (const iterator of arg) {
+//     if(iterator > 0){
+//       res1.push(iterator)
+
+//     }
+//   }
+//   return res1
+// }
+// console.log(name(res));
+
+// 14 misol
+
+// let res1 = ['salom', 'xayr', 'xayrli kun'];
+
+// function name(arr) {
+//     return arr.map(res => res.length);
+// }
+
+// console.log(name(res1));
+
+// 15 misol
+
+// let res = [1,2,3,4,5];
+// function name(arg) {
+//   let sum = 0;
+//   for (const iterator of arg) {
+//     sum += iterator **2
+
+//   }
+//   return sum
+// }
+// console.log(name(res));
+
+// 16 misol
+// 17 misol
+
+//     let str = [1,2,3,4,5,6];
+
+// function sum(arg) {
+//     let reversed = [];
+
+//     for (let i = arg.length - 1; i >= 0; i--) {
+//         reversed += arg[i];
+//     }
+
+//     return reversed;
+// }
+// console.log(sum(str));
+
+// 18 misol
+// 19 misol
+// 20 misol
+
+// let arg = ['salom','xayr','olma'];
+// function name(arg) {
+//     let sum = arg.map(function(v){
+//      return v[0].toUpperCase() + v.slice(1)
+
+//     })
+//      return sum
+// }
+// console.log(name(arg));
+
+// 21 misol
+
+// let str = 'salom                                                                                   xayr';
+// function name(arg) {
+//     return arg.split(' ').join('');
+// }
+
+// console.log(name(str));
+
+// 22 misol
+
+// let res = 'salom';
+// function name(arg) {
+//     let sum = arg.split('');
+//     res  = sum.reverse()
+//     let arr = res.join('')
+//     return arr
+    
+// }
+// console.log(name(res));
+
+// 23 misol
+
+
